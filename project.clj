7760ccb07240ad695baa41c5e808855cba02d708
mandy1339 <mandy1339@gmail.com>
2017-08-16 23:20:36 -0400

    I think first commit

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [toledohue "0.1.0-SNAPSHOT"]]
+                 [toledohue "0.1.1"]]
   :main turn-on.core
   :aot [turn-on.core])
--- src/turn_on/core.clj
+++ src/turn_on/core.clj
@@ -1,11 +1,12 @@
 (ns turn-on.core
-  (:require [toledohue.hue :as hue])
+  (:require [toledohue.hue :refer [get-light-info]])
+  (:use [toledohue.hue])
   (:gen-class))
 
 
 (defn -main
   "turns on my hue light"
   []
-  )
+  (turn-on 1 (get-user)))
 
 
